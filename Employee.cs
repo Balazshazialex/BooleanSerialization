646f5f7b21d3c2eee138e8911093f87784974327
Balazshazialex <58106361+Balazshazialex@users.noreply.github.com>
2022-10-23 22:58:33 +0300

    Basic structure and solution

---
+++ Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanSerialization
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public bool IsBranchManager { get; set; }
+
+        public override string ToString()
+        {
+            return "Name:" + Name + ", Salary:" + Salary + ", IsBranchManager:" + IsBranchManager;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+
+namespace BooleanSerialization
+{
+    public class MainProgram
+    {
+        public static void Main()
+        {
+            Employee employee = new Employee { Name = "John Doe", Salary = 2000, IsBranchManager = false };
+            var jsonString = JsonConvert.SerializeObject(employee, Formatting.None,
+                                                                   new JsonSerializerSettings
+                                                                   {
+                                                                       Converters = new List<JsonConverter>
+                                                                    {
+                                                                        new CustomJsonConverter()
+                                                                    }
+                                                                   });
+            Console.WriteLine("Serialized object: " + jsonString);
+
+            var string1 = "{\"Name\":\"John Doe\",\"Salary\":2000,\"IsBranchManager\":0}";
+            var obj1 = JsonConvert.DeserializeObject<Employee>(string1,
+                                                                   new JsonSerializerSettings
+                                                                   {
+                                                                       Converters = new List<JsonConverter>
+                                                                    {
+                                                                        new CustomJsonConverter()
+                                                                    }
+                                                                   });
+            Console.WriteLine("Deserialized object: " + obj1);
+            var string2 = "{\"Name\":\"John Doe\",\"Salary\":2000,\"IsBranchManager\":\"yes\"}";
+            var obj2 = JsonConvert.DeserializeObject<Employee>(string2,
+                                                                   new JsonSerializerSettings
+                                                                   {
+                                                                       Converters = new List<JsonConverter>
+                                                                    {
+                                                                        new CustomJsonConverter()
+                                                                    }
+                                                                   });
+            Console.WriteLine("Deserialized object: " + obj2);
+        }
+    }
+}
